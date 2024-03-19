0127b0c0440c4559d22cd853044826beb4046834
loginglug <157985345+loginglug@users.noreply.github.com>
2024-03-19 12:19:44 +0530

    Initial commit

---
+++ admin/src/App.jsx
@@ -0,0 +1,18 @@
+import{Browse
+}
+import './App.css'
+import Cards from './components/card.jsx'
+function App() {
+  
+
+  return (
+    <div >
+     hello world
+<Cards icon={"icon1"} text={"text1"} image={"image1"}/>
+<Cards icon={"icon2"} text={"text2"} image={"image2"}/>
+<Cards icon={"icon3"} text={"text3"} image={"image3"}/>
+    </div>
+  )
+}
+
+export default App
---
+++ admin/src/card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Cards(props){
+    return(
+        <div className="flex">
+        <div className="">[props.icon]</div>
+        <div className="">[props.text]</div>
+        <div className="">[props.image]</div>
+        </div>
+    )
+}
+export default Cards
